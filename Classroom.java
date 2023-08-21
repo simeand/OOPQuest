0459f36c8ddeb2d39fd9ae44412d6618af8552fd
simeand <122308541+simeand@users.noreply.github.com>
2023-08-21 16:23:25 +0200

    Create Classroom.java

---
+++ Classroom.java
@@ -0,0 +1,19 @@
+public class Classroom extends Wilder{
+    public static void main(String[] args) {
+        Wilder student1 = new Wilder();
+        student1.setFirstName("Mark");
+        student1.setIsPresent(true);
+
+        Wilder student2 = new Wilder();
+        student2.setFirstName("Musk");
+        student2.setIsPresent(false);
+
+        System.out.println(student1.whoAmI());
+        System.out.println(student2.whoAmI());
+    }
+}
+
+
+// OUTPUT
+//   My name is Mark and I am present
+//   My name is Musk and I am not present
